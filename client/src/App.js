f41615749daf0c805b4f842b0743b934438d44f5
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-04-21 14:44:05 +0530

    error page done

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 //! Components
-import Landing from "./pages/Landing";
+import { Dashboard, Error, Landing, Register } from "./pages";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<div>Dashboard</div>}></Route>
-                <Route path="/register" element={<div>register</div>}></Route>
+                <Route path="/" element={<Dashboard />}></Route>
+                <Route path="/register" element={<Register />}></Route>
                 <Route path="/landing" element={<Landing />}></Route>
-                <Route path="*" element={<div>Error</div>}></Route>
+                <Route path="*" element={<Error />}></Route>
             </Routes>
         </BrowserRouter>
     );
