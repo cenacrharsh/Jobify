8c1db8e150bd41bf7059a2ad8cc5564a6a04dcd1
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-04-21 14:30:25 +0530

    setting up react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 //! Components
 import Landing from "./pages/Landing";
 
 function App() {
     return (
-        <div>
-            <Landing />
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<div>Dashboard</div>}></Route>
+                <Route path="/register" element={<div>register</div>}></Route>
+                <Route path="/landing" element={<Landing />}></Route>
+                <Route path="*" element={<div>Error</div>}></Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import main from "../assets/images/main.svg";
 
 import Wrapper from "../assets/wrappers/LandingPage";
@@ -23,7 +25,9 @@ const Landing = () => {
                         Praesentium molestiae veniam delectus distinctio
                         voluptatem enim minima quaerat.
                     </p>
-                    <button className="btn btn-hero">Login/Register</button>
+                    <Link to="/register" className="btn btn-hero">
+                        Login/Register
+                    </Link>
                 </div>
                 <img src={main} alt="job hunt" className="img main-img" />
             </div>
