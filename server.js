3eda7afaa4ee61bde50ef784d6fdfefa72b26730
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-04-29 20:57:16 +0530

    error handler middleware setup

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 import express from "express";
 import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
 
 const app = express();
-
 
 const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
+    throw new Error("error");
     return res.send("Welcome To Jobify Backend !!!");
 });
 
 //! Middleware
 app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT :: ${PORT}`);
