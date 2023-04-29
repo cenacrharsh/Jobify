124d0e0c1000be2933e6bb8787880533335a7cf9
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-04-29 20:50:05 +0530

    not found middleware set

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 import express from "express";
+import notFoundMiddleware from "./middleware/not-found.js";
 
 const app = express();
+
 
 const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
     return res.send("Welcome To Jobify Backend !!!");
 });
+
+//! Middleware
+app.use(notFoundMiddleware);
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT :: ${PORT}`);
