2f3b46073c2c07afcf68f31ef59a7fc71d5c766f
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-07-06 10:06:00 +0530

    using express-async-errors package to avoid try-catch

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,8 @@
 import User from "../models/User.js";
 
-const register = async (req, res) => {
-    try {
-        const user = await User.create(req.body);
-        res.status(201).json({ user });
-    } catch (error) {
-        res.status(500).json({ message: "There was an error" });
-    }
+const register = async (req, res, next) => {
+    const user = await User.create(req.body);
+    res.status(201).json({ user });
 };
 
 const login = async (req, res) => {
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,7 +1,7 @@
 const errorHandlerMiddleware = (err, req, res, next) => {
     console.log(err);
     res.status(500).json({
-        msg: "There was an error",
+        message: err,
     });
 };
 
