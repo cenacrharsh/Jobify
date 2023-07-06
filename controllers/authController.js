90c5255b06242cc66ffcb289c8fa985e17329a9d
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-07-06 09:52:57 +0530

    register controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,12 @@
+import User from "../models/User.js";
+
 const register = async (req, res) => {
-    res.send("Register User");
+    try {
+        const user = await User.create(req.body);
+        res.status(201).json({ user });
+    } catch (error) {
+        res.status(500).json({ message: "There was an error" });
+    }
 };
 
 const login = async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,39 +1,44 @@
 import mongoose from "mongoose";
 import validator from "validator";
 
-const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "Please provide name"],
-        minLength: 2,
-        maxLength: 20,
-        trim: true,
-    },
-    email: {
-        type: String,
-        required: [true, "Please provide email"],
-        validate: {
-            validator: validator.isEmail,
-            message: "Please provide a valid email",
+const UserSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please provide name"],
+            minLength: 2,
+            maxLength: 20,
+            trim: true,
         },
-        unique: true,
+        email: {
+            type: String,
+            required: [true, "Please provide email"],
+            validate: {
+                validator: validator.isEmail,
+                message: "Please provide a valid email",
+            },
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: [true, "Please provide password"],
+        },
+        lastName: {
+            type: String,
+            trim: true,
+            maxLength: 20,
+            default: "lastName",
+        },
+        location: {
+            type: String,
+            trim: true,
+            maxLength: 20,
+            default: "my city",
+        },
     },
-    password: {
-        type: String,
-        required: [true, "Please provide password"],
-    },
-    lastName: {
-        type: String,
-        trim: true,
-        maxLength: 20,
-        default: "lastName",
-    },
-    location: {
-        type: String,
-        trim: true,
-        maxLength: 20,
-        default: "my city",
-    },
-});
+    {
+        timestamps: true,
+    }
+);
 
 export default mongoose.model("User", UserSchema);
