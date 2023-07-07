8798367d9ecdafb7eb473b1e063040203d05d1db
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-07-07 22:54:06 +0530

    set up BadRequestError and NotFoundError

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,20 @@ import { StatusCodes } from "http-status-codes";
 class CustomAPIError extends Error {
     constructor(message) {
         super(message);
+    }
+}
+
+class BadRequestError extends CustomAPIError {
+    constructor(message) {
+        super(message);
         this.statusCode = StatusCodes.BAD_REQUEST;
+    }
+}
+
+class NotFoundError extends CustomAPIError {
+    constructor(message) {
+        super(message);
+        this.statusCode = StatusCodes.NOT_FOUND;
     }
 }
 
@@ -12,7 +25,7 @@ const register = async (req, res, next) => {
     const { name, email, password } = req.body;
 
     if (!name || !email || !password) {
-        throw new CustomAPIError("Please provide all values");
+        throw new BadRequestError("Please provide all values");
     }
 
     const user = await User.create({ name, email, password });
