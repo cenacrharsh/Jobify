7cdef710b4f0e91914cfb110e3c83968bcb54403
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-07-07 15:02:14 +0530

    added validation error handling

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -4,10 +4,23 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     console.log(err);
     const defaultError = {
         statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-        message: "Something went wrong, Try again later",
+        msg: "Something went wrong, Try again later",
     };
+
+    //> Validation Error
+    if (err.name === "ValidationError") {
+        defaultError.statusCode = StatusCodes.BAD_REQUEST;
+        defaultError.msg = Object.values(err.errors)
+            .map((item) => item.message)
+            .join(", ");
+        // defaultError.msg = err.message;
+    }
+
+    // res.status(defaultError.statusCode).json({
+    //     msg: err,
+    // });
     res.status(defaultError.statusCode).json({
-        message: err,
+        msg: defaultError.msg,
     });
 };
 
