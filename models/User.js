fd99955936a07c4580e97c1dcf1c9f4ddd3219e1
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-07-08 23:55:30 +0530

    hash passwords before storing

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import bcrypt from "bcryptjs";
 
 const UserSchema = new mongoose.Schema(
     {
@@ -40,5 +41,10 @@ const UserSchema = new mongoose.Schema(
         timestamps: true,
     }
 );
+
+UserSchema.pre("save", async function () {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
 
 export default mongoose.model("User", UserSchema);
