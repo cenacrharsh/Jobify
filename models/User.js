b26c4ceef5ad7289821fe7bac5e28f74dca484a1
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-07-09 14:15:28 +0530

    move jwt to env

--- models/User.js
+++ models/User.js
@@ -49,7 +49,9 @@ UserSchema.pre("save", async function () {
 });
 
 UserSchema.methods.createJWT = function () {
-    return jwt.sign({ userId: this._id }, "jwtSecret", { expiresIn: "1d" });
+    return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_LIFETIME,
+    });
 };
 
 export default mongoose.model("User", UserSchema);
