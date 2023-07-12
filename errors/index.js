08be79d32e09706a6046c929caa06b7067f35c11
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-07-12 23:14:14 +0530

    unauthenticated error

--- errors/index.js
+++ errors/index.js
@@ -1,4 +1,5 @@
 import NotFoundError from "./not-found.js";
 import BadRequestError from "./bad-request.js";
+import UnAuthenticatedError from "./unauthenticated.js";
 
-export { NotFoundError, BadRequestError };
+export { NotFoundError, BadRequestError, UnAuthenticatedError };
